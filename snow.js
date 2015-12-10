c1c1c27ff52dbdff48fcc850fdbd452c2e24cc94
54sword <54sword@gmail.com>
2015-12-10 13:06:08 +0800

    更新

--- snow.js
+++ snow.js
@@ -1,6 +1,7 @@
 /*
 
 Snow({
+  img: 'http://localhost:8888/images/snow.png',
   y: '85px',
   x: '50px',
   width: '300px',
@@ -39,6 +40,8 @@ var Snow = function(config) {
   var snowSize = config.snowSize || 20;
 
   var interval = config.interval || 400;
+
+  var image = config.image;
 
   var snow = (function() {
 
@@ -157,5 +160,51 @@ var Snow = function(config) {
 
   timer();
   createSnow();
+
+
+  // http://www.cnblogs.com/2050/p/4029656.html
+  function addCSS(cssText) {
+    var style = document.createElement('style'),  //创建一个style元素
+      head = document.head || document.getElementsByTagName('head')[0]; //获取head元素
+      style.type = 'text/css'; //这里必须显示设置style元素的type属性为text/css，否则在ie中不起作用
+
+    if (style.styleSheet) { //IE
+      var func = function() {
+        try{ //防止IE中stylesheet数量超过限制而发生错误
+          style.styleSheet.cssText = cssText;
+        } catch(e) {
+        }
+      }
+      //如果当前styleSheet还不能用，则放到异步中则行
+      if (style.styleSheet.disabled) {
+        setTimeout(func, 10);
+      } else {
+        func();
+      }
+    } else { //w3c
+      //w3c浏览器中只要创建文本节点插入到style元素中就行了
+      var textNode = document.createTextNode(cssText);
+      style.appendChild(textNode);
+    }
+    head.appendChild(style); //把创建的style元素插入到head中
+  };
+
+  var css = '.snow{'+
+    'background-image: url('+image+');'+
+    'background-repeat: no-repeat;'+
+    'position: absolute;'+
+    'background-size:100% 100%;'+
+    'filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='+image+',  sizingMethod=\'scale\');'+
+    'animation-name: spin;'+
+    'animation-duration: 4000ms;'+
+    'animation-iteration-count: infinite;'+
+    'animation-timing-function: linear;'+
+  '}'+
+  '@keyframes spin {'+
+    'from { transform: rotate(0deg); }'+
+    'to { transform: rotate(360deg); }'+
+  '}';
+
+  addCSS(css);
 
 };
